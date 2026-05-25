2a8cdf613ca78844a72f40f955998678435618a3
propgateai-sys <propgate.ai@gmail.com>
2026-05-25 11:14:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ def clean_text(text):
     return text.strip()
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/generator')
 def index():
     return render_template('index.html')
 
